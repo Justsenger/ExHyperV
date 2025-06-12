2bff37a11809f1a561d2cba5147ca19e371b6b54
HryshcIlya <175040986+HryshcIlya@users.noreply.github.com>
2025-06-12 13:49:05 +0500

    Restore accidentally deleted comments

--- src/Views/Pages/ChooseGPUWindow.xaml.cs
+++ src/Views/Pages/ChooseGPUWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.ObjectModel;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
 using System.Management.Automation;
@@ -51,10 +51,10 @@ public partial class ChooseGPUWindow : FluentWindow
         var selectedGpu = GpuListView.SelectedItem as GPU;
         if (selectedGpu != null)
         {
-            progress.IsIndeterminate = true;
+            progress.IsIndeterminate = true; //等待条
 
             var result = await Task.Run(() => GPUMount(Machinename, selectedGpu.Path, selectedGpu.Manu));
-
+            //接下来，将执行真正的GPU分配。
             if (result == "running")
             {
                 ContentDialog Dialog = new()
@@ -65,7 +65,7 @@ public partial class ChooseGPUWindow : FluentWindow
                 };
                 Dialog.DialogHost = ContentPresenterForDialogs;
 
-                await Dialog.ShowAsync(CancellationToken.None);
+                await Dialog.ShowAsync(CancellationToken.None); //显示提示框
             }
             else if (result == "error")
             {
@@ -77,12 +77,12 @@ public partial class ChooseGPUWindow : FluentWindow
                 };
                 Dialog.DialogHost = ContentPresenterForDialogs;
 
-                await Dialog.ShowAsync(CancellationToken.None);
+                await Dialog.ShowAsync(CancellationToken.None); //显示提示框
             }
             else if (result == "OK")
             {
-                progress.IsIndeterminate = false;
-                GPUSelected?.Invoke(this, (selectedGpu.GPUname, Machinename));
+                progress.IsIndeterminate = false; //等待条结束
+                GPUSelected?.Invoke(this, (selectedGpu.GPUname, Machinename)); // 触发事件
             }
         }
 
@@ -94,6 +94,7 @@ public partial class ChooseGPUWindow : FluentWindow
     {
         var listView = sender as ListView;
 
+        // 如果有选中的项，启用 "确定" 按钮
         if (listView.SelectedItem != null)
             ConfirmButton.IsEnabled = true;
         else
@@ -106,19 +107,21 @@ public partial class ChooseGPUWindow : FluentWindow
         {
             var ps = PowerShell.Create();
 
+            //1.先检测VM是否关闭，没有关机则停止操作。
             ps.AddScript($@"(Get-VM -Name '{vmname}').State");
-            if (ps.Invoke()[0].ToString() != "Off") return "running";
-
+            if (ps.Invoke()[0].ToString() != "Off") return "running"; //没关机
+            //2.设定缓存接管和低位内存=128和高位内存=32G。（关机已通过）
             ps.AddScript($"Set-VM -GuestControlledCacheTypes $true -VMName '{vmname}'");
             ps.Invoke();
             ps.AddScript(
-                $"Set-VM -HighMemoryMappedIoSpace 32GB –VMName '{vmname}'");
+                $"Set-VM -HighMemoryMappedIoSpace 32GB –VMName '{vmname}'"); //设定32G已经很大了，据说准确的设定是基于显存向上取整的最大二进制数，比如33G就要设定为64G了
             ps.Invoke();
-            ps.AddScript($"Set-VM -LowMemoryMappedIoSpace 128MB -VMName '{vmname}'");
+            ps.AddScript($"Set-VM -LowMemoryMappedIoSpace 128MB -VMName '{vmname}'"); //设定为128MB，祖传别动，否则USB直通受影响
             ps.Invoke();
-            ps.AddScript($"Get-VM -VMName '{vmname}' | select *");
+            ps.AddScript($"Get-VM -VMName '{vmname}' | select *"); //还得全部查找刷新一下
             ps.Invoke();
 
+            //3.添加GPU分区，使用默认参数。这是因为消费级硬件，或者非Grid驱动的显卡，无法限制虚拟机资源使用，只能竞争。
             ps.AddScript($"Add-VMGpuPartitionAdapter -VMName '{vmname}' -InstancePath '{gpupath}'");
             ps.Invoke();
             ps.Commands.Clear();
@@ -126,11 +129,16 @@ public partial class ChooseGPUWindow : FluentWindow
                 $"Set-VMGpuPartitionAdapter -VMName '{vmname}' -MinPartitionVRAM 80000000 -MaxPartitionVRAM 100000000 -OptimalPartitionVRAM 100000000 -MinPartitionEncode 80000000 -MaxPartitionEncode 100000000 -OptimalPartitionEncode 100000000 -MinPartitionDecode 80000000 -MaxPartitionDecode 100000000 -OptimalPartitionDecode 100000000 -MinPartitionCompute 80000000 -MaxPartitionCompute 100000000 -OptimalPartitionCompute 100000000");
             ps.Invoke();
 
+
+            //4.驱动复制，直接把整个"C:\Windows\System32\DriverStore\FileRepository"拿进虚拟机。
+
             //todo分支，如果未找到路径，不报错，而是转为linux注入模式，要求输入用户名和密码等待ssh链接。
 
+            //获取虚拟机系统盘路径，一般默认为第一块。
             ps.AddScript($"(Get-VMHardDiskDrive -vmname '{vmname}')[0].Path");
             var harddiskpath = ps.Invoke()[0].ToString();
 
+            //挂载硬盘并寻找第一个系统分区的盘符
             ps.AddScript(@$"
             $VHD = Mount-VHD -Path '{harddiskpath}' -PassThru
             $VHD | Get-Disk | Get-Partition | Where-Object {{ -not $_.DriveLetter }} | Add-PartitionAccessPath -AssignDriveLetter
@@ -144,12 +152,13 @@ public partial class ChooseGPUWindow : FluentWindow
             }}
             ");
 
-            var letter = ps.Invoke()[0].ToString();
+            var letter = ps.Invoke()[0].ToString(); //仅仅是一个字母
 
 
             var sourceFolder = @"C:\Windows\System32\DriverStore\FileRepository";
             var destinationFolder = letter + @":\Windows\System32\HostDriverStore\FileRepository";
 
+            // 创建目标文件夹（如果不存在）
             if (!Directory.Exists(destinationFolder)) Directory.CreateDirectory(destinationFolder);
 
             var process = new Process
@@ -167,12 +176,13 @@ public partial class ChooseGPUWindow : FluentWindow
             process.BeginOutputReadLine();
             process.WaitForExit();
 
-            SetFolderReadOnly(destinationFolder);
+            SetFolderReadOnly(destinationFolder); // 设置目标文件夹及其所有文件为只读属性，防止nvlddmkm文件丢失
 
 
+            //对于N卡，需要修补注册表信息：nvlddmkm
             if (manu.Contains("NVIDIA")) NvidiaReg(letter + ":");
 
-            ps.AddScript($"Dismount-VHD -Path '{harddiskpath}'");
+            ps.AddScript($"Dismount-VHD -Path '{harddiskpath}'"); //卸载磁盘
             ps.Invoke();
 
             return "OK";
@@ -186,11 +196,14 @@ public partial class ChooseGPUWindow : FluentWindow
 
     private static void SetFolderReadOnly(string folderPath)
     {
+        // 设置文件夹属性为只读
         var directoryInfo = new DirectoryInfo(folderPath);
         directoryInfo.Attributes |= FileAttributes.ReadOnly;
 
+        // 设置所有子文件夹的属性为只读
         foreach (var subFolder in Directory.GetDirectories(folderPath)) SetFolderReadOnly(subFolder);
 
+        // 设置所有文件的属性为只读
         foreach (var file in Directory.GetFiles(folderPath))
             File.SetAttributes(file, File.GetAttributes(file) | FileAttributes.ReadOnly);
     }
@@ -210,6 +223,8 @@ public partial class ChooseGPUWindow : FluentWindow
         File.WriteAllText(tempRegFile, regContent);
         RunPsScript($@"reg import ""{tempRegFile}"""); // 导入
         RunPsScript("reg unload HKLM\\OfflineSystem"); // 卸载
+
+        //注册表应该是要修复，否则nvlddmkm.sys会莫名丢失。
     }
 
     private void RunPsScript(string script)
