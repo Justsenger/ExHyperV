6137486d7f6b7cd1c0c8905073e0f0aea51b97ce
Justsenger <76653335+Justsenger@users.noreply.github.com>
2025-11-14 00:15:44 +0800

    Win10 GPU-PV实现，测试通过。

--- src/Services/DiskParserService.cs
+++ src/Services/DiskParserService.cs
@@ -1,127 +1,137 @@
 ﻿using System.IO;
-public class DiskParserService
+namespace ExHyperV.Services
 {
-    private static readonly Guid WindowsBasicDataGuid = new Guid("EBD0A0A2-B9E5-4433-87C0-68B6B72699C7");
-    private static readonly Guid LinuxFileSystemGuid = new Guid("0FC63DAF-8483-4772-8E79-3D69D8477DE4");
-    private static readonly Guid EfiSystemGuid = new Guid("C12A7328-F81F-11D2-BA4B-00A0C93EC93B");
-
-    public List<PartitionInfo> GetPartitions(string devicePath)
+    public class DiskParserService
     {
-        var partitions = new List<PartitionInfo>();
+        private static readonly Guid WindowsBasicDataGuid = new Guid("EBD0A0A2-B9E5-4433-87C0-68B6B72699C7");
+        private static readonly Guid LinuxFileSystemGuid = new Guid("0FC63DAF-8483-4772-8E79-3D69D8477DE4");
+        private static readonly Guid EfiSystemGuid = new Guid("C12A7328-F81F-11D2-BA4B-00A0C93EC93B");
 
-        using (var diskStream = new FileStream(devicePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+        public List<PartitionInfo> GetPartitions(string devicePath)
         {
-            var mbrBuffer = new byte[512];
-            diskStream.Read(mbrBuffer, 0, 512);
+            var partitions = new List<PartitionInfo>();
 
-            if (BitConverter.ToUInt16(mbrBuffer, 510) != 0xAA55)
+            using (var diskStream = new FileStream(devicePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
             {
-                return partitions;
+                var mbrBuffer = new byte[512];
+                diskStream.Read(mbrBuffer, 0, 512);
+
+                if (BitConverter.ToUInt16(mbrBuffer, 510) != 0xAA55)
+                {
+                    return partitions;
+                }
+
+                if (IsGptProtectiveMbr(mbrBuffer))
+                {
+                    partitions.AddRange(ParseGptPartitions(diskStream));
+                }
+                else
+                {
+                    partitions.AddRange(ParseMbrPartitions(mbrBuffer));
+                }
             }
 
-            if (IsGptProtectiveMbr(mbrBuffer))
+            const long oneGbInBytes = 1024L * 1024 * 1024;
+            var filteredPartitions = partitions
+                .Where(p => p.SizeInBytes >= oneGbInBytes)
+                .ToList();
+
+            return filteredPartitions;
+        }
+
+        private bool IsGptProtectiveMbr(byte[] mbrBuffer)
+        {
+            for (int i = 0; i < 4; i++)
             {
-                partitions.AddRange(ParseGptPartitions(diskStream));
+                int offset = 446 + (i * 16);
+                if (mbrBuffer[offset + 4] == 0xEE)
+                {
+                    return true;
+                }
             }
-            else
+            return false;
+        }
+
+        private IEnumerable<PartitionInfo> ParseMbrPartitions(byte[] mbrBuffer)
+        {
+            for (int i = 0; i < 4; i++)
             {
-                partitions.AddRange(ParseMbrPartitions(mbrBuffer));
+                int offset = 446 + (i * 16);
+                byte systemId = mbrBuffer[offset + 4];
+
+                if (systemId == 0x00) continue;
+
+                uint startSector = BitConverter.ToUInt32(mbrBuffer, offset + 8);
+                uint totalSectors = BitConverter.ToUInt32(mbrBuffer, offset + 12);
+                ulong startOffset = (ulong)startSector * 512;
+                ulong size = (ulong)totalSectors * 512;
+
+                var (osType, description) = GetOsTypeFromMbrId(systemId);
+                yield return new PartitionInfo(i + 1, startOffset, size, osType, description);
             }
         }
 
-        return partitions;
-    }
-
-    private bool IsGptProtectiveMbr(byte[] mbrBuffer)
-    {
-        for (int i = 0; i < 4; i++)
+        private IEnumerable<PartitionInfo> ParseGptPartitions(FileStream diskStream)
         {
-            int offset = 446 + (i * 16);
-            if (mbrBuffer[offset + 4] == 0xEE)
+            diskStream.Seek(512, SeekOrigin.Begin);
+            var gptHeaderBuffer = new byte[512];
+            diskStream.Read(gptHeaderBuffer, 0, 512);
+
+            if (BitConverter.ToUInt64(gptHeaderBuffer, 0) != 0x5452415020494645)
             {
-                return true;
+                yield break;
+            }
+
+            ulong partitionArrayStartLba = BitConverter.ToUInt64(gptHeaderBuffer, 72);
+            uint partitionEntryCount = BitConverter.ToUInt32(gptHeaderBuffer, 80);
+            uint partitionEntrySize = BitConverter.ToUInt32(gptHeaderBuffer, 84);
+
+            diskStream.Seek((long)(partitionArrayStartLba * 512), SeekOrigin.Begin);
+
+            for (int i = 0; i < partitionEntryCount; i++)
+            {
+                var entryBuffer = new byte[partitionEntrySize];
+                diskStream.Read(entryBuffer, 0, (int)partitionEntrySize);
+
+                var typeGuid = new Guid(entryBuffer.Take(16).ToArray());
+
+                if (typeGuid == Guid.Empty) continue;
+
+                ulong firstLba = BitConverter.ToUInt64(entryBuffer, 32);
+                ulong lastLba = BitConverter.ToUInt64(entryBuffer, 40);
+                ulong startOffset = firstLba * 512;
+                ulong size = (lastLba - firstLba + 1) * 512;
+
+                var (osType, description) = GetOsTypeFromGptGuid(typeGuid);
+                yield return new PartitionInfo(i + 1, startOffset, size, osType, description);
             }
         }
-        return false;
-    }
 
-    private IEnumerable<PartitionInfo> ParseMbrPartitions(byte[] mbrBuffer)
-    {
-        for (int i = 0; i < 4; i++)
+        private (OperatingSystemType, string) GetOsTypeFromMbrId(byte systemId)
         {
-            int offset = 446 + (i * 16);
-            byte systemId = mbrBuffer[offset + 4];
-            if (systemId == 0x00) continue;
-
-            uint startSector = BitConverter.ToUInt32(mbrBuffer, offset + 8);
-            uint totalSectors = BitConverter.ToUInt32(mbrBuffer, offset + 12);
-            ulong startOffset = (ulong)startSector * 512;
-            ulong size = (ulong)totalSectors * 512;
-
-            var (osType, description) = GetOsTypeFromMbrId(systemId);
-            yield return new PartitionInfo(i + 1, startOffset, size, osType, description);
-        }
-    }
-
-    private IEnumerable<PartitionInfo> ParseGptPartitions(FileStream diskStream)
-    {
-        diskStream.Seek(512, SeekOrigin.Begin);
-        var gptHeaderBuffer = new byte[512];
-        diskStream.Read(gptHeaderBuffer, 0, 512);
-
-        if (BitConverter.ToUInt64(gptHeaderBuffer, 0) != 0x5452415020494645)
-        {
-            yield break;
+            switch (systemId)
+            {
+                case 0x07: // NTFS
+                    return (OperatingSystemType.Windows, "Windows (NTFS)");
+                case 0x83: // Linux
+                    return (OperatingSystemType.Linux, "Linux");
+                case 0xEF: // EFI
+                    return (OperatingSystemType.EFI, "EFI System Partition");
+                default:
+                    return (OperatingSystemType.Unknown, $"Unknown (Type 0x{systemId:X2})");
+            }
         }
 
-        ulong partitionArrayStartLba = BitConverter.ToUInt64(gptHeaderBuffer, 72);
-        uint partitionEntryCount = BitConverter.ToUInt32(gptHeaderBuffer, 80);
-        uint partitionEntrySize = BitConverter.ToUInt32(gptHeaderBuffer, 84);
-
-        diskStream.Seek((long)(partitionArrayStartLba * 512), SeekOrigin.Begin);
-
-        for (int i = 0; i < partitionEntryCount; i++)
+        private (OperatingSystemType, string) GetOsTypeFromGptGuid(Guid typeGuid)
         {
-            var entryBuffer = new byte[partitionEntrySize];
-            diskStream.Read(entryBuffer, 0, (int)partitionEntrySize);
-
-            var typeGuid = new Guid(entryBuffer.Take(16).ToArray());
-            if (typeGuid == Guid.Empty) continue;
-
-            ulong firstLba = BitConverter.ToUInt64(entryBuffer, 32);
-            ulong lastLba = BitConverter.ToUInt64(entryBuffer, 40);
-            ulong startOffset = firstLba * 512;
-            ulong size = (lastLba - firstLba + 1) * 512;
-
-            var (osType, description) = GetOsTypeFromGptGuid(typeGuid);
-            yield return new PartitionInfo(i + 1, startOffset, size, osType, description);
-        }
-    }
-
-    private (OperatingSystemType, string) GetOsTypeFromMbrId(byte systemId)
-    {
-        switch (systemId)
-        {
-            case 0x07:
-                return (OperatingSystemType.Windows, "Windows (NTFS)");
-            case 0x83:
-                return (OperatingSystemType.Linux, "Linux");
-            case 0xEF:
+            if (typeGuid == WindowsBasicDataGuid)
+                return (OperatingSystemType.Windows, "Windows (Basic Data)");
+            if (typeGuid == LinuxFileSystemGuid)
+                return (OperatingSystemType.Linux, "Linux Filesystem");
+            if (typeGuid == EfiSystemGuid)
                 return (OperatingSystemType.EFI, "EFI System Partition");
-            default:
-                return (OperatingSystemType.Linux, $"Linux (Type {systemId:X2})");
+
+            return (OperatingSystemType.Unknown, "Unknown Partition Type");
         }
-    }
-
-    private (OperatingSystemType, string) GetOsTypeFromGptGuid(Guid typeGuid)
-    {
-        if (typeGuid == WindowsBasicDataGuid)
-            return (OperatingSystemType.Windows, "Windows (Basic Data)");
-        if (typeGuid == LinuxFileSystemGuid)
-            return (OperatingSystemType.Linux, "Linux Filesystem");
-        if (typeGuid == EfiSystemGuid)
-            return (OperatingSystemType.EFI, "EFI System Partition");
-
-        return (OperatingSystemType.Linux, "Linux (Unknown Type)");
     }
 }
--- src/Services/GpuPartitionService.cs
+++ src/Services/GpuPartitionService.cs
@@ -9,6 +9,8 @@ namespace ExHyperV.Services
     public class GpuPartitionService : IGpuPartitionService
     {
 
+        private bool IsWindows11OrGreater() => Environment.OSVersion.Version.Build >= 22000;
+
         // PowerShell 脚本常量
         private const string GetGpuWmiInfoScript = "Get-CimInstance -Class Win32_VideoController | select PNPDeviceID,name,AdapterCompatibility,DriverVersion";
         private const string GetGpuRamScript = @"
@@ -26,9 +28,14 @@ namespace ExHyperV.Services
                           }
                       }} |
                 Where-Object { $_.MemorySize -ne $null -and $_.MemorySize -gt 0 }";
-        private const string GetPartitionableGpusScript = "Get-VMHostPartitionableGpu | select name";
+
+        private const string GetPartitionableGpusWin11Script = "Get-VMHostPartitionableGpu | select name";
+        private const string GetPartitionableGpusWin10Script = "Get-VMPartitionableGpu | select name"; 
+
+
+
         private const string CheckHyperVModuleScript = "Get-Module -ListAvailable -Name Hyper-V";
-        private const string GetVmsScript = "Hyper-V\\Get-VM | Select vmname,LowMemoryMappedIoSpace,GuestControlledCacheTypes,HighMemoryMappedIoSpace";
+        private const string GetVmsScript = "Hyper-V\\Get-VM | Select vmname,LowMemoryMappedIoSpace,GuestControlledCacheTypes,HighMemoryMappedIoSpace,Notes";
 
         public Task<string> GetVmStateAsync(string vmName)
         {
@@ -138,7 +145,6 @@ namespace ExHyperV.Services
 
             return normalizedId;
         }
-
         public Task<List<GPUInfo>> GetHostGpusAsync()
         {
             return Task.Run(() =>
@@ -185,6 +191,7 @@ namespace ExHyperV.Services
                     }
                 }
 
+                string GetPartitionableGpusScript = IsWindows11OrGreater() ? GetPartitionableGpusWin11Script : GetPartitionableGpusWin10Script;
                 var partitionableGpus = Utils.Run(GetPartitionableGpusScript);
                 if (partitionableGpus.Count > 0)
                 {
@@ -221,6 +228,7 @@ namespace ExHyperV.Services
                         string vmname = vm.Members["VMName"]?.Value?.ToString() ?? string.Empty;
                         string highmmio = vm.Members["HighMemoryMappedIoSpace"]?.Value?.ToString() ?? string.Empty;
                         string guest = vm.Members["GuestControlledCacheTypes"]?.Value?.ToString() ?? string.Empty;
+                        string notes = vm.Members["Notes"]?.Value?.ToString() ?? string.Empty;
 
                         var vmgpus = Utils.Run($@"Get-VMGpuPartitionAdapter -VMName '{vmname}' | Select InstancePath,Id");
                         if (vmgpus.Count > 0)
@@ -229,6 +237,22 @@ namespace ExHyperV.Services
                             {
                                 string gpupath = gpu.Members["InstancePath"]?.Value?.ToString() ?? string.Empty;
                                 string gpuid = gpu.Members["Id"]?.Value?.ToString() ?? string.Empty;
+                                // 如果InstancePath为空 (Win10场景)，则尝试从备注中解析
+                                if (string.IsNullOrEmpty(gpupath) && !string.IsNullOrEmpty(notes))
+                                {
+                                    string tagPrefix = "[AssignedGPU:";
+                                    int startIndex = notes.IndexOf(tagPrefix);
+                                    if (startIndex != -1)
+                                    {
+                                        startIndex += tagPrefix.Length;
+                                        int endIndex = notes.IndexOf("]", startIndex);
+                                        if (endIndex != -1)
+                                        {
+                                            gpupath = notes.Substring(startIndex, endIndex - startIndex);
+                                        }
+                                    }
+                                }
+
                                 gpulist[gpuid] = gpupath;
                             }
                         }
@@ -238,7 +262,7 @@ namespace ExHyperV.Services
                 return vmList;
             });
         }
-        private bool IsWindows11OrGreater() => Environment.OSVersion.Version.Build >= 22000;
+        
         private async Task<string> InjectWindowsDriversAsync(string vmName, string harddiskpath, PartitionInfo partition, string gpuManu)
         {
             string assignedDriveLetter = null;
@@ -349,79 +373,121 @@ namespace ExHyperV.Services
         {
             return Task.Run(async () =>
             {
+                bool isWin10 = !IsWindows11OrGreater();
                 var disabledGpuInstanceIds = new List<string>();
+                string NormalizeForComparison(string deviceId)
+                {
+                    if (string.IsNullOrWhiteSpace(deviceId)) return string.Empty;
+                    var normalizedId = deviceId.Replace('#', '\\').ToUpper();
+                    if (normalizedId.StartsWith(@"\\?\")) normalizedId = normalizedId.Substring(4);
+                    int suffixIndex = normalizedId.IndexOf('{');
+                    if (suffixIndex != -1)
+                    {
+                        int lastSeparatorIndex = normalizedId.LastIndexOf('\\', suffixIndex);
+                        if (lastSeparatorIndex != -1)
+                        {
+                            normalizedId = normalizedId.Substring(0, lastSeparatorIndex);
+                        }
+                    }
+                    return normalizedId;
+                }
+                void ShowMessageOnUIThread(string message, string title = "提示")
+                {
+                    System.Windows.Application.Current.Dispatcher.Invoke(() => Utils.Show2(message));
+                }
 
                 try
                 {
                     Utils.AddGpuAssignmentStrategyReg();
-                    var vmStateResult = Utils.Run($"(Hyper-V\\Get-VM -Name '{vmName}').State");
-                    if (vmStateResult == null || vmStateResult.Count == 0)
+                    var vmStateResult = Utils.Run($"(Get-VM -Name '{vmName}').State");
+                    if (vmStateResult == null || vmStateResult.Count == 0 || vmStateResult[0].ToString() != "Off")
                     {
-                        return string.Format(Properties.Resources.GetVmState_Error, vmName);
+                        return $"错误：虚拟机 '{vmName}' 必须处于关闭状态。";
                     }
-                    if (vmStateResult[0].ToString() != "Off")
-                    {
-                        return ExHyperV.Properties.Resources.Running;
-                    }
-
-                    if (!IsWindows11OrGreater())
+                    if (isWin10)
                     {
                         var allHostGpus = await GetHostGpusAsync();
+                        string normalizedSelectedGpuId = NormalizeForComparison(gpuInstancePath);
+
                         foreach (var gpu in allHostGpus)
                         {
-                            if (gpu.InstanceId != gpuInstancePath && gpu.InstanceId.ToUpper().StartsWith("PCI\\"))
+                            if (!gpu.InstanceId.ToUpper().StartsWith("PCI\\")) continue;
+
+                            string normalizedCurrentGpuId = NormalizeForComparison(gpu.InstanceId);
+
+                            if (!string.Equals(normalizedCurrentGpuId, normalizedSelectedGpuId, StringComparison.OrdinalIgnoreCase))
                             {
-                                Utils.Run($"Disable-PnpDevice -InstanceId '{gpu.InstanceId}' -Confirm:$false");
                                 disabledGpuInstanceIds.Add(gpu.InstanceId);
                             }
                         }
-                        await Task.Delay(3000);
+
+                        if (disabledGpuInstanceIds.Any())
+                        {
+                            foreach (var id in disabledGpuInstanceIds)
+                            {
+                                Utils.Run($"Disable-PnpDevice -InstanceId '{id}' -Confirm:$false");
+                            }
+                            await Task.Delay(2000);
+                        }
                     }
-
-                    string addGpuCommand = IsWindows11OrGreater()
-                        ? $"Add-VMGpuPartitionAdapter -VMName '{vmName}' -InstancePath '{gpuInstancePath}'"
-                        : $"Add-VMGpuPartitionAdapter -VMName '{vmName}'";
-
+                    string addGpuCommand = isWin10
+                        ? $"Add-VMGpuPartitionAdapter -VMName '{vmName}'"
+                        : $"Add-VMGpuPartitionAdapter -VMName '{vmName}' -InstancePath '{gpuInstancePath}'";
+                    
                     string vmConfigScript = $@"
-                Set-VM -GuestControlledCacheTypes $true -VMName '{vmName}'
-                Set-VM -HighMemoryMappedIoSpace 64GB –VMName '{vmName}'
-                Set-VM -LowMemoryMappedIoSpace 1GB -VMName '{vmName}'
-                {addGpuCommand}
-            ";
+                        Set-VM -GuestControlledCacheTypes $true -VMName '{vmName}';
+                        Set-VM -HighMemoryMappedIoSpace 64GB –VMName '{vmName}';
+                        Set-VM -LowMemoryMappedIoSpace 1GB -VMName '{vmName}';
+                        {addGpuCommand};
+                        ";
                     Utils.Run(vmConfigScript);
+
+                    string gpuTag = $"[AssignedGPU:{gpuInstancePath}]";
+                    string updateNotesScript = $@"
+                    $vm = Get-VM -Name '{vmName}';
+                    $currentNotes = $vm.Notes;
+                    $cleanedNotes = $currentNotes -replace '\[AssignedGPU:[^\]]+\]', '';
+                    $newNotes = ($cleanedNotes.Trim() + ' ' + '{gpuTag}').Trim();
+                    Set-VM -VM $vm -Notes $newNotes;
+                    ";
+                    Utils.Run(updateNotesScript);
 
                     var harddiskPathResult = Utils.Run($"(Get-VMHardDiskDrive -vmname '{vmName}')[0].Path");
                     if (harddiskPathResult == null || harddiskPathResult.Count == 0)
                     {
-                        return string.Format(Properties.Resources.Error_CannotGetVmHardDiskPath, vmName);
+                        return "错误：无法获取虚拟机硬盘路径以注入驱动。";
                     }
                     string harddiskpath = harddiskPathResult[0].ToString();
-
-                    if (selectedPartition.OsType == OperatingSystemType.Windows)
+                    string injectionResult = await InjectWindowsDriversAsync(vmName, harddiskpath, selectedPartition, gpuManu);
+                    if (injectionResult != "OK") return injectionResult;
+                    if (isWin10)
                     {
-                        return await InjectWindowsDriversAsync(vmName, harddiskpath, selectedPartition, gpuManu);
+                        Utils.Run($"Start-VM -Name '{vmName}'");
                     }
-                    else if (selectedPartition.OsType == OperatingSystemType.Linux)
-                    {
-                        return "SSH_REQUIRED";
-                    }
-                    else
-                    {
-                        return $"错误：不支持为所选分区类型（{selectedPartition.TypeDescription}）注入驱动程序。";
-                    }
+                    return "OK"; 
                 }
                 catch (Exception ex)
                 {
-                    return string.Format(Properties.Resources.Error_UnexpectedSystemException, ex.Message);
+                    ShowMessageOnUIThread($"【严重异常】\n\n操作中发生异常: {ex.Message}");
+                    return $"操作失败: {ex.Message}";
                 }
                 finally
                 {
                     if (disabledGpuInstanceIds.Any())
                     {
+                        await Task.Delay(1000);
                         foreach (var instanceId in disabledGpuInstanceIds)
                         {
                             Utils.Run($"Enable-PnpDevice -InstanceId '{instanceId}' -Confirm:$false");
                         }
+                    }
+                    if (isWin10)
+                    {
+                        string finalMessage = "GPU配置完成，虚拟机已启动。\n\n" +
+                                              "由于Windows10的固有缺陷，本次指定的GPU分区仅在本次启动时有效。\n\n" +
+                                              "若要确保下次冷启动仍然是该GPU，请先手动禁用其他GPU，或再次通过本工具重新分配。";
+                        System.Windows.Application.Current.Dispatcher.Invoke(() => Utils.Show(finalMessage));
+
                     }
                 }
             });
@@ -431,7 +497,20 @@ namespace ExHyperV.Services
             return Task.Run(() =>
             {
                 var results = Utils.Run2($@"Remove-VMGpuPartitionAdapter -VMName '{vmName}' -AdapterId '{adapterId}' -Confirm:$false");
-                return results != null;
+                if (results != null)
+                {
+                    string cleanupNotesScript = $@"
+                    $vm = Get-VM -Name '{vmName}';
+                    $currentNotes = $vm.Notes;
+                    if ($currentNotes -match '\[AssignedGPU:[^\]]+\]') {{
+                    $cleanedNotes = $currentNotes -replace '\[AssignedGPU:[^\]]+\]', '';
+                    Set-VM -VM $vm -Notes $cleanedNotes.Trim();
+                        }}
+                    ";
+                    Utils.Run(cleanupNotesScript);
+                    return true;
+                }
+                return false;
             });
         }
 
