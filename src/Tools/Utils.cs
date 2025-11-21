c8ea4fdaf4680ddac08f4f4a083030294679da07
Justsenger <76653335+Justsenger@users.noreply.github.com>
2025-11-21 21:19:32 +0800

    UI优化

--- src/Tools/Utils.cs
+++ src/Tools/Utils.cs
@@ -79,7 +79,7 @@ public class Utils
         switch (deviceType)
         {
             case "Switch":
-                return "\xE967";  // 交换机图标 
+                return "\xF597";  // 交换机图标 
             case "Upstream":
                 return "\uE774";  // 地球/上游网络图标
             case "Display":
