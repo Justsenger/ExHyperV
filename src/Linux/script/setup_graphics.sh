5073061217dfb9c6d8273f912b4dd8079045d01b
Justsenger <76653335+Justsenger@users.noreply.github.com>
2025-11-25 16:55:49 +0800

    fix-mesa23

--- src/Linux/script/setup_graphics.sh
+++ src/Linux/script/setup_graphics.sh
@@ -18,6 +18,13 @@ echo "[+] (Graphics) Adding Kisak PPA for Vulkan (v25)..."
 sudo add-apt-repository ppa:kisak/turtle -y
 sudo apt-get update -qq
 
+echo "[+] (Graphics) Applying Safety Lock for OpenGL (Force v23)..."
+sudo bash -c 'cat > /etc/apt/preferences.d/00-mesa-hold-gl <<EOF
+Package: libgl1-mesa-dri libglapi-mesa libglx-mesa0 libgbm1
+Pin: release o=Ubuntu
+Pin-Priority: 1001
+EOF'
+
 echo "[+] (Graphics) Configuring APT Pinning to lock Vulkan to Kisak PPA..."
 # 核心逻辑：只让 mesa-vulkan-drivers 使用 kisak 源
 sudo bash -c 'cat > /etc/apt/preferences.d/99-mesa-pinning <<EOF
