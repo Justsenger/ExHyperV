63031947e6bc831c6b4fb87261f03aff4fa21bba
Justsenger <76653335+Justsenger@users.noreply.github.com>
2025-12-24 22:05:50 +0800

    CPU 虚拟机过多时，列表撑大的问题修复。

--- src/Views/CpuPage.xaml.cs
+++ src/Views/CpuPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using ExHyperV.ViewModels;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace ExHyperV.Views.Pages
 {
@@ -9,6 +11,23 @@ namespace ExHyperV.Views.Pages
         {
             InitializeComponent();
             DataContext = CpuPageViewModel.Instance;
+
+            // 监听加载事件
+            this.Loaded += CpuPage_Loaded;
+        }
+
+        private void CpuPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            DependencyObject parent = VisualTreeHelper.GetParent(this);
+            while (parent != null)
+            {
+                if (parent is ScrollViewer parentScrollViewer)
+                {
+                    parentScrollViewer.VerticalScrollBarVisibility = ScrollBarVisibility.Disabled;
+                    break;
+                }
+                parent = VisualTreeHelper.GetParent(parent);
+            }
         }
     }
 }
