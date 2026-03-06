7dad7db988a760bf5b1d91d8501834955dcb1d26
Justsenger <76653335+Justsenger@users.noreply.github.com>
2026-03-06 13:03:26 +0800

    命名空间修复

--- src/Models/LinuxScriptItem.cs
+++ src/Models/LinuxScriptItem.cs
@@ -1,12 +1,15 @@
-﻿public class LinuxScriptItem
+﻿namespace ExHyperV.Models
 {
-    public string Name { get; set; } = "Unknown";
-    public string Description { get; set; } = string.Empty;
-    public string Author { get; set; } = "Unknown";
-    public string Version { get; set; } = "1.0.0";
-    public bool IsLocal { get; set; }
-    public string SourcePathOrUrl { get; set; } = string.Empty;
-    public string FileName { get; set; } = string.Empty;
+    public class LinuxScriptItem
+    {
+        public string Name { get; set; } = "Unknown";
+        public string Description { get; set; } = string.Empty;
+        public string Author { get; set; } = "Unknown";
+        public string Version { get; set; } = "1.0.0";
+        public bool IsLocal { get; set; }
+        public string SourcePathOrUrl { get; set; } = string.Empty;
+        public string FileName { get; set; } = string.Empty;
 
-    public override string ToString() => Name;
+        public override string ToString() => Name;
+    }
 }
