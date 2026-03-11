fc7e649deb04844d1d9545da8a266470dda6ed6e
Justsenger <76653335+Justsenger@users.noreply.github.com>
2026-03-11 22:46:25 +0800

    控制台完工

--- src/Views/Components/VmConsoleView.xaml.cs
+++ src/Views/Components/VmConsoleView.xaml.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Diagnostics;
-using System.Windows.Controls;
-using ExHyperV.ViewModels;
+﻿using ExHyperV.ViewModels;
 
 namespace ExHyperV.Views.Components
 {
@@ -11,7 +8,6 @@ namespace ExHyperV.Views.Components
         {
             InitializeComponent();
 
-            // 监听 DataContext 变化，这样当 ViewModel 加载时，View 自动绑定事件
             this.DataContextChanged += (s, e) =>
             {
                 if (e.OldValue is ConsoleViewModel oldVm)
@@ -19,7 +15,6 @@ namespace ExHyperV.Views.Components
 
                 if (e.NewValue is ConsoleViewModel newVm)
                 {
-                    Debug.WriteLine("[View] 已成功订阅 ViewModel 的 CAD 事件");
                     newVm.SendCadRequested += OnSendCadRequested;
                 }
             };
@@ -33,14 +28,11 @@ namespace ExHyperV.Views.Components
 
         private void OnSendCadRequested(object? sender, EventArgs e)
         {
-            // 当 ViewModel 触发事件时，直接调用下面的公共方法
             this.SendCtrlAltDel();
         }
 
-        // --- 核心：必须定义这个公共方法，外部(如Window)才能调用，且编译器才不会报错 ---
         public void SendCtrlAltDel()
         {
-            Debug.WriteLine("[View] 正在转发 CAD 指令给底层 RdpHost");
             RdpHost?.SendCtrlAltDel();
         }
     }
--- src/Views/ConsoleWindow.xaml.cs
+++ src/Views/ConsoleWindow.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Windows;
-using Wpf.Ui.Controls;
+﻿using Wpf.Ui.Controls;
 using ExHyperV.ViewModels;
 using System.Windows.Input;
 
@@ -12,16 +10,9 @@ namespace ExHyperV.Views
 
         public ConsoleWindow(string vmId, string vmName)
         {
-            // 1. 准备数据：使用我们完善后的构造函数
             _viewModel = new ConsoleViewModel(vmId, vmName);
-
-            // 2. 核心：在加载 UI 前绑定，确保子控件能够通过继承获取 DataContext
             this.DataContext = _viewModel;
-
-            // 3. 初始化组件
             InitializeComponent();
-
-            // 4. 设置窗口标题
             this.Title = vmName;
         }
 
@@ -30,8 +21,6 @@ namespace ExHyperV.Views
         /// </summary>
         private void OnSendCadRequested(object? sender, EventArgs e)
         {
-            // ConsoleHost 是 XAML 中指定的 <components:VmConsoleView x:Name="ConsoleHost" />
-            // 调用该组件内部封装好的发送 CAD 方法
             ConsoleHost?.SendCtrlAltDel();
         }
 
