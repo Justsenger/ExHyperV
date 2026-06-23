5feef4cdd92bb9dc79b782de7a48b5d66918044c
Justsenger <shalingye@gmail.com>
2026-06-23 10:56:32 +0800

    refactor: MainPageViewModel 删死命令 OnNavigate + 死属性 Author/BuildDate

--- src/ViewModels/MainPageViewModel.cs
+++ src/ViewModels/MainPageViewModel.cs
@@ -1,9 +1,5 @@
-﻿using System.Globalization;
 using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
-using ExHyperV.Interaction;
 using ExHyperV.Services;
-using ExHyperV.Views;
 
 namespace ExHyperV.ViewModels
 {
@@ -14,14 +10,10 @@ namespace ExHyperV.ViewModels
         [ObservableProperty] private string? _cpuModel;
         [ObservableProperty] private string? _memCap;
         [ObservableProperty] private string? _appVersion;
-        [ObservableProperty] private string? _author;
-        [ObservableProperty] private string? _buildDate;
 
         public MainPageViewModel()
         {
             AppVersion = AppInfoService.Version;
-            Author = AppInfoService.Author;
-            BuildDate = AppInfoService.BuildTime.ToString("yyyy/MM/dd HH:mm", CultureInfo.InvariantCulture);
             _ = LoadSystemInfoAsync();
         }
 
@@ -32,23 +24,6 @@ namespace ExHyperV.ViewModels
             OSArchitecture = info.OSArchitecture;
             CpuModel = info.CpuModel;
             MemCap = info.MemCap;
-        }
-
-        [RelayCommand]
-        private void OnNavigate(string parameter)
-        {
-            Type? pageType = parameter switch
-            {
-                "VM" => typeof(VirtualMachinesPage),
-                "Host" => typeof(HostPage),
-                "PCIe" => typeof(PCIePage),
-                "Network" => typeof(SwitchPage),
-                "USB" => typeof(USBPage),
-                _ => null
-            };
-
-            if (pageType != null)
-                Navigation.NavigateTo(pageType);
         }
     }
 }
