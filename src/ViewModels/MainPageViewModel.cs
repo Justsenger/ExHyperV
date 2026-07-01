7fc09b51e534db20ea5e13b7c108c65e30939862
Justsenger <shalingye@gmail.com>
2026-07-02 00:36:14 +0800

    Revert "Update MainPageViewModel.cs"
    This reverts commit 785a575e9a3927dc5f3aff4f13a5280147a4ea18.

--- src/ViewModels/MainPageViewModel.cs
+++ src/ViewModels/MainPageViewModel.cs
@@ -1,6 +1,9 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using ExHyperV.Interaction;
 using ExHyperV.Services;
 using ExHyperV.Tools;
+using ExHyperV.Views;
 
 namespace ExHyperV.ViewModels
 {
@@ -25,6 +28,23 @@ namespace ExHyperV.ViewModels
             OSArchitecture = info.OSArchitecture;
             CpuModel = info.CpuModel;
             MemCap = info.MemCap;
+        }
+
+        [RelayCommand]
+        private void OnNavigate(string parameter)
+        {
+            Type? pageType = parameter switch
+            {
+                "VM" => typeof(VirtualMachinesPage),
+                "Host" => typeof(HostPage),
+                "PCIe" => typeof(PCIePage),
+                "Network" => typeof(SwitchPage),
+                "USB" => typeof(USBPage),
+                _ => null
+            };
+
+            if (pageType != null)
+                Navigation.NavigateTo(pageType);
         }
     }
 }
